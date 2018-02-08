223ef125fb2572e9f65eebf2e370a0733b2f6077
Ignotos <ignotomichael@gmail.com>
2018-02-07 22:47:55 -0500

    conversion from cad to btc working

--- app/src/main/java/com/cryptoconverter/app/MainActivity.java
+++ app/src/main/java/com/cryptoconverter/app/MainActivity.java
@@ -1,24 +1,34 @@
 package com.cryptoconverter.app;
 
+import android.app.AlertDialog;
+import android.content.Context;
+import android.content.DialogInterface;
+import android.os.Build;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.EditText;
 import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
     Button click;
-    public static TextView data;
+    public static EditText convertedAmountTextBox;
+    public static EditText inputAmountTextBox;
+    public static AlertDialog.Builder builder;
+    //public static Context context;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
 
         super.onCreate(savedInstanceState);
+        //context = getApplicationContext();
         setContentView(R.layout.activity_main);
 
         click = findViewById(R.id.button);
-        data = findViewById(R.id.fetcheddata);
+        inputAmountTextBox = findViewById(R.id.inputAmountTextBox);
+        convertedAmountTextBox = findViewById(R.id.convertedAmountTextBox);
 
         click.setOnClickListener(new View.OnClickListener() {
 
